a7f14ac655d03e1ffac7c57a9ab44b7d5f50a9d2
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-25 18:42:42 -0700

    Update CommandUI.php

--- src/IceCruelStuff/ExplosiveSnowballs/CommandUI.php
+++ src/IceCruelStuff/ExplosiveSnowballs/CommandUI.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
+ *
+ *   _____          _____                 _  _____ _          __  __ 
+ *  |_   _|        / ____|               | |/ ____| |        / _|/ _|
+ *    | |  ___ ___| |     _ __ _   _  ___| | (___ | |_ _   _| |_| |_ 
+ *    | | / __/ _ \ |    | '__| | | |/ _ \ |\___ \| __| | | |  _|  _|
+ *   _| || (_|  __/ |____| |  | |_| |  __/ |____) | |_| |_| | | | |  
+ *  |_____\___\___|\_____|_|   \__,_|\___|_|_____/ \__|\__,_|_| |_|  
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @author IceCruelStuff
+ * @link https://github.com/IceCruelStuff/ExplosiveSnowballs
+ *
+*/
+
 declare(strict_types=1);
 
 namespace IceCruelStuff\ExplosiveSnowballs;
