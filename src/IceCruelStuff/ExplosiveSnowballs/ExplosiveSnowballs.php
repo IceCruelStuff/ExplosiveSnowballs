869eaebd314ad0dcbbf46f8111977fa6af9bb840
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-25 20:25:45 -0700

    Update ExplosiveSnowballs.php

--- src/IceCruelStuff/ExplosiveSnowballs/ExplosiveSnowballs.php
+++ src/IceCruelStuff/ExplosiveSnowballs/ExplosiveSnowballs.php
@@ -35,6 +35,9 @@ use pocketmine\level\Position;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+use pocketmine\nbt\JsonNBTParser;
+use pocketmine\item\Item;
+use pocketmine\utils\TextFormat;
 use IceCruelStuff\ExplosiveSnowballs\CommandUI;
 use function count;
 
@@ -52,14 +55,21 @@ class ExplosiveSnowballs extends PluginBase implements Listener {
         }
         $this->config = new Config($this->getDataFolder() . 'config.yml', Config::YAML, array(
             "disable-explosive-snowballs" => false,
-            "disable-ui" => true
+            "disable-ui" => true,
+            "default-snowball-name" => "Explosive Snowball"
         ));
         $this->config->save();
         if (!$this->config->get("disable-explosive-snowballs")) {
             $this->config->set("disable-explosive-snowballs", false);
+            $this->config->save();
         }
         if (!$this->config->get("disable-ui")) {
             $this->config->set("disable-ui", true);
+            $this->config->save();
+        }
+        if (!$this->config->get("default-snowball-name")) {
+            $this->config->set("default-snowball-name", "Explosive Snowball");
+            $this->config->save();
         }
     }
 
@@ -76,29 +86,54 @@ class ExplosiveSnowballs extends PluginBase implements Listener {
                         case "enable":
                         case "on":
                             if ($this->config->get("disable-explosive-snowballs") == false) {
-                                $sender->sendMessage("ExplosiveSnowballs are already enabled");
+                                $sender->sendMessage(TextFormat::RED . "ExplosiveSnowballs are already enabled");
                                 return false;
                             } elseif (!$this->config->get("disable-explosive-snowballs") == false) {
                                 $this->config->set("disable-explosive-snowballs", false);
                                 $this->config->save();
-                                $sender->sendMessage("ExplosiveSnowballs are now enabled");
+                                $sender->sendMessage(TextFormat::GREEN . "ExplosiveSnowballs are now enabled");
                             }
                             break;
                         case "disable":
                         case "off":
                             if ($this->config->get("disable-explosive-snowballs") == true) {
-                                $sender->sendMessage("ExplosiveSnowballs are already disabled");
+                                $sender->sendMessage(TextFormat::RED . "ExplosiveSnowballs are already disabled");
                                 return false;
                             } elseif (!$this->config->get("disable-explosive-snowballs") == true) {
                                 $this->config->set("disable-explosive-snowballs", true);
                                 $this->config->save();
-                                $sender->sendMessage("ExplosiveSnowballs are now disabled");
+                                $sender->sendMessage(TextFormat::GREEN . "ExplosiveSnowballs are now disabled");
                             }
                             break;
                     }
                 } else {
-                    $sender->sendMessage("You do not have the permission to use this command");
+                    $sender->sendMessage(TextFormat::RED . "You do not have the permission to use this command");
                 }
+                break;
+            case "givesnowball":
+                if ($sender->hasPermission("snowballs.give")) {
+                    if (isset($args[0])) {
+                        $name = $args[0];
+                        $target = $this->getServer()->getPlayer($name);
+                        if ($target instanceof Player) {
+                            $itemName = "Explosive Snowball";
+                            if (isset($args[1])) {
+                                $itemName = implode(array_slice($args, 3), " ");
+                            }
+                            $item = Item::get(Item::SNOWBALL, 0, 1, JsonNBTParser::parseJSON("{display:Name:{$itemName}}"));
+                            $target->getInventory()->addItem($item);
+                            $sender->sendMessage(TextFormat::GREEN . "Gave " . $target->getName() . $item->getName());
+                            return true;
+                        } else {
+                            $sender->sendMessage(TextFormat::RED . $name . " not found");
+                            return true;
+                        }
+                    }
+                    return false;
+                } else {
+                    $sender->sendMessage(TextFormat::RED . "You do not have permission to use this command");
+                }
+                break;
         }
         return true;
     }
