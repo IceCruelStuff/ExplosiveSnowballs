f27568cd652393aa3fae11f101ad3b0d981b8a73
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-10-02 22:53:37 -0700

    Update CommandUI.php

--- src/IceCruelStuff/ExplosiveSnowballs/CommandUI.php
+++ src/IceCruelStuff/ExplosiveSnowballs/CommandUI.php
@@ -30,7 +30,7 @@ use jojoe77777\FormAPI\CustomForm;
 use jojoe77777\FormAPI\SimpleForm;
 use IceCruelStuff\ExplosiveSnowballs\ExplosiveSnowballs as Main;
 
-class CommandUI extends Form {
+class CommandUI implements Form {
 
     public $form;
     public $player;
