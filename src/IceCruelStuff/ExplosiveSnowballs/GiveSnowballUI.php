34c49e6dca312251aafd89b51425c11e9f56c23d
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-10-04 21:36:09 -0700

    Create GiveSnowballUI.php

---
+++ src/IceCruelStuff/ExplosiveSnowballs/GiveSnowballUI.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ *
+ *   _____          _____                 _  _____ _          __  __ 
+ *  |_   _|        / ____|               | |/ ____| |        / _|/ _|
+ *    | |  ___ ___| |     _ __ _   _  ___| | (___ | |_ _   _| |_| |_ 
+ *    | | / __/ _ \ |    | '__| | | |/ _ \ |\___ \| __| | | |  _|  _|
+ *   _| || (_|  __/ |____| |  | |_| |  __/ |____) | |_| |_| | | | |  
+ *  |_____\___\___|\_____|_|   \__,_|\___|_|_____/ \__|\__,_|_| |_|  
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @author IceCruelStuff
+ * @link https://github.com/IceCruelStuff/ExplosiveSnowballs
+ *
+*/
+
+declare(strict_types=1);
+
+namespace IceCruelStuff\ExplosiveSnowballs;
+
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+use jojoe77777\FormAPI\CustomForm;
+use jojoe77777\FormAPI\SimpleForm;
+
+class GiveSnowballUI {
+/*
+    private $plugin;
+
+    public function __construct($plugin) {
+        $this->plugin = $plugin;
+    }
+
+    public function sendCustomForm($sender) {
+        $customForm = new CustomForm(function (Player $customPlayer, array $customData = null) {
+            if ($data === null) {
+                return;
+            }
+
+            switch ($data) {
+                case 0:
+                    
+            }
+        });
+        $customForm->setTitle(TextFormat::AQUA . 'Explosive Snowballs');
+        $customForm->setLabel('Give Explosive Snowballs to players!');
+        $customForm->addInput('Player name', 'Steve');
+        $customForm->addInput('Custom name', 'Explosive Snowball');
+        $customForm->addSlider('Amount', 1, 100);
+        $customForm->sendToPlayer($sender);
+    }
+*/
+}
